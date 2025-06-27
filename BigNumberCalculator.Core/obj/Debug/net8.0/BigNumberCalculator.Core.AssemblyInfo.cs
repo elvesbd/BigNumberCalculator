f4071c9fc9654246997f2c960a189d2566611c94
elves <elves.damasceno@rarolabs.com.br>
2025-06-27 09:18:30 -0300

    docs: add readme

--- BigNumberCalculator.Core/obj/Debug/net8.0/BigNumberCalculator.Core.AssemblyInfo.cs
+++ BigNumberCalculator.Core/obj/Debug/net8.0/BigNumberCalculator.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BigNumberCalculator.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f92f23a87be10ae78b32a603728b29001c3e649")]
 [assembly: System.Reflection.AssemblyProductAttribute("BigNumberCalculator.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BigNumberCalculator.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
